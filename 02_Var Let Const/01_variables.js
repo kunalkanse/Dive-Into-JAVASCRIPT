edf98dde037b4e8459b5b12226b820a474e6a5d6
kunal kanse <92772714+kunalkanse@users.noreply.github.com>
2023-12-27 23:40:18 +0530

    Add files via upload

---
+++ 02_Var Let Const/01_variables.js
@@ -0,0 +1,28 @@
+
+const accountId = 144553;
+let accountEmail = "kanseks008@gmail.com";
+var accountPassword = "123456";
+
+accountCity = "pune";   // loosely typed language
+
+let accountState;
+
+// Not Allowed for const
+/* 
+    accountId = 2;
+*/
+
+/* 
+    prefer not to use var
+    because of issue in block scope & functional scope
+    {} => scope
+*/
+
+// reinitialise
+accountEmail = "abc@gmail.com";
+accountPassword = "312645";
+accountCity = "mumbai";
+
+console.log(accountId);
+
+console.table([accountId , accountEmail , accountPassword , accountCity , accountState]);
