7f242eadcea25ec27fa510529ef0a3971d973dc7
kunal kanse <92772714+kunalkanse@users.noreply.github.com>
2023-12-28 22:17:35 +0530

    Add files via upload

---
+++ 03_Datatypes/primitives/2_string methods/strm.js
@@ -0,0 +1,66 @@
+
+let a = "Good Morning"
+
+let b = a.length
+console.log(b)
+
+console.log(String.fromCharCode(65));
+
+let c = a.charAt(3)
+console.log(c)
+
+let d = a.charCodeAt(2)
+console.log(d)
+
+let x = a.toUpperCase()
+console.log(x)
+
+let y = a.toLowerCase()
+console.log(y)
+
+let z = a.startsWith('G')
+console.log(z)
+
+let w = a.startsWith('g')
+console.log(w)
+
+let k = a.endsWith('g')
+console.log(k)
+
+console.log(x.endsWith('G'))
+
+let e = a.includes('M')
+console.log(e)
+
+let f = a.indexOf('n')
+console.log(f)
+
+let g = a.repeat(5)
+console.log(g)
+
+let h = a.replace('Morning','night')
+console.log(h)
+
+let i = a.valueOf()
+console.log(i)
+
+let j = a.slice(0,4)
+console.log(j)
+
+let l = a.slice(5)
+console.log(l)
+
+let m = a.split()
+console.log(m)
+
+let n = a.split('')
+console.log(n)
+
+let o = a.split(' ')
+console.log(o)
+
+let p = a.split('o')
+console.log(p)
+
+let q = a.concat(" Everyone")
+console.log(q)
