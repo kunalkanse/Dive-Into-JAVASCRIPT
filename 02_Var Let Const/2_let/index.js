f11eb08135a9ad8d7ebc258c4a5488f9472fc71a
kunal kanse <92772714+kunalkanse@users.noreply.github.com>
2023-12-28 22:09:51 +0530

    Add files via upload

---
+++ 02_Var Let Const/2_let/index.js
@@ -0,0 +1,29 @@
+// Declaration
+let a
+console.log(a)
+
+
+// initialization
+let b
+b = 10
+console.log(b)
+
+// Decleration & initialization
+let c = 20
+console.log(c)
+
+// Re-initialization
+let d
+d = 20
+d = 30
+console.log(d)
+
+// NOT ALLOWED : Re-decleration & Re-initialization
+
+// let f = 20
+// let f=50
+// console.log(f)
+
+// Hoisting
+// console.log(x)  // ERROR
+let x = 100
