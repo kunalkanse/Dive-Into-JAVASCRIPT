b6625a2ae2b7701dd12ee68d51599e9a5bbcb81d
kunal kanse <92772714+kunalkanse@users.noreply.github.com>
2023-12-28 22:16:02 +0530

    Add files via upload

---
+++ 03_Datatypes/primitives/1_string/str.js
@@ -0,0 +1,19 @@
+
+let a = "i am kunal"
+console.log(a)
+console.log(typeof a)
+
+let b = " i'm kunal "
+console.log(b)
+
+let c = 'i am "Kunal" '
+console.log(c)
+
+let d = ` i'm "Kunal" `
+console.log(d)
+
+let name = "Kunal"
+let age = 23
+
+console.log(`my name is ${name} and my age is ${age} `)
+console.log(`my name is ${name} my age is ${age} `)
