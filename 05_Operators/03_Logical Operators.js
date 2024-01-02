59694050337a644318b7c9d53db9c98bac7e5590
kunal kanse <92772714+kunalkanse@users.noreply.github.com>
2024-01-02 23:44:33 +0530

    Add files via upload

---
+++ 05_Operators/03_Logical Operators.js
@@ -0,0 +1,27 @@
+
+////////////////////////////////////
+// Logical Operators
+const hasDriversLicense = true; // A
+const hasGoodVision = true; // B
+
+console.log(hasDriversLicense && hasGoodVision);
+console.log(hasDriversLicense || hasGoodVision);
+console.log(!hasDriversLicense);
+
+/*
+hasGoodVision = false;
+if (hasDriversLicense && hasGoodVision) {
+  console.log('Sarah is able to drive!');
+} else {
+  console.log('Someone else should drive...');
+}
+*/
+
+const isTired = false; // C
+console.log(hasDriversLicense && hasGoodVision && isTired);
+
+if (hasDriversLicense && hasGoodVision && !isTired) {
+    console.log('Sarah is able to drive!');
+} else {
+    console.log('Someone else should drive...');
+}
