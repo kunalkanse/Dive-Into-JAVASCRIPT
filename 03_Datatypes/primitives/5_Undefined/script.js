47e0222c2779133e21ee4fa40c552e1fb12c3aa9
kunal kanse <92772714+kunalkanse@users.noreply.github.com>
2024-01-03 22:26:41 +0530

    Add files via upload

---
+++ 03_Datatypes/primitives/5_Undefined/script.js
@@ -0,0 +1,22 @@
+
+let a = undefined
+console.log(a)
+console.log(typeof a)
+
+let b
+console.log(b)
+console.log(typeof b)
+
+let c = undefined;
+console.log(a);
+console.log(typeof a);
+
+let d = undefined;
+//let d = 15;
+console.log(d);
+console.log(typeof d); 
+
+// undefined is one of the primitive datatype.
+// udefined is a variabe that has not been assigned value that is undefined
+// declared a variable non assigned value.
+// undefied is local property of local object like window in browser.
