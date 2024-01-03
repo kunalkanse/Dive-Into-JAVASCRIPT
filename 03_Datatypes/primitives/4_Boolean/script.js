71b17d7cd102446a12a466859d0f4d824b9981e5
kunal kanse <92772714+kunalkanse@users.noreply.github.com>
2024-01-03 22:24:59 +0530

    Add files via upload

---
+++ 03_Datatypes/primitives/4_Boolean/script.js
@@ -0,0 +1,13 @@
+
+let a = true
+console.log(a)
+console.log(typeof a)
+
+
+let b = false
+console.log(b)
+console.log(typeof b)
+
+// Boolean is javascript primitive dtatype.
+// boolean can hold two values true or false
+// it it used for controlling program from like if, if-else,
