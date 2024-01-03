442c2df964dcbf53fe53384f44c19b28ca23f671
kunal kanse <92772714+kunalkanse@users.noreply.github.com>
2024-01-03 22:21:03 +0530

    Add files via upload

---
+++ 03_Datatypes/primitives/3_numbers/script.js
@@ -0,0 +1,21 @@
+
+let a = 10
+console.log(a)  // 10
+console.log(typeof (a))  // Number
+
+let b = 10.5
+console.log(b)  // 10.5
+console.log(typeof b)   // Number
+
+let pi = 3.146465465
+let x = pi.toFixed(2)   // implicite typecasting
+console.log(x)  // 3.15
+console.log(typeof x)   // string
+
+let y = parseInt("10.654").toString()   // explicite typecasting
+console.log(y)  //10
+console.log(typeof y)   //String
+
+let z = parseFloat("10.644")
+console.log(z)  // 10.644
+console.log(typeof z)   // Number
