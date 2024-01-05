65b2eda9d99db54a0d02472149e82359d784c83d
kunal kanse <92772714+kunalkanse@users.noreply.github.com>
2024-01-05 22:51:54 +0530

    Add files via upload

---
+++ 03_Datatypes/primitives/6_null/index.js
@@ -0,0 +1,7 @@
+let a = null
+console.log(a)
+console.log(typeof a)
+
+// null is one of the primitive datatype .
+// null represents absence of object value.
+// null is not global property of global object.
