4e947f00c60e5a59b3b75fa765a9d46ebd67af2a
kunal kanse <92772714+kunalkanse@users.noreply.github.com>
2024-01-05 22:55:29 +0530

    Add files via upload

---
+++ 06_Different output methods/index.js
@@ -0,0 +1,24 @@
+console.log("Hello")
+
+console.error("i am ERROR")
+
+console.warn("i am WARNING")
+
+alert("I AM ALERT")
+
+confirm("I  aM CONFIRM")
+
+document.writeln("kunal")
+document.write("kanse")
+document.writeln("baramati")
+document.write("kanse")
+
+let age = prompt("please enter your age")
+
+if (age >= 18) {
+    alert("you are 18 crossed")
+    confirm("you are eligible continue to vote")
+}
+else {
+    alert("you are not eligible")
+}
