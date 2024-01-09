631e1a02cce912050991589aeda88ea0085c9501
kunal kanse <92772714+kunalkanse@users.noreply.github.com>
2024-01-09 23:56:18 +0530

    Create script.js

---
+++ 07_Arrays/01_array/script.js
@@ -0,0 +1,22 @@
+
+// In javascript we can store any type of data inside Arrays
+let a = ["Hello", 2, true, undefined, null, function(){},[],{} , 100]
+
+///////////////////////////////////////////////////////////////////////////
+let emp = ['jonas','thakur',2001,'developer',['kunal','omkar', 'rohit']];
+
+console.log(a);
+console.log(typeof a);  // Object
+
+console.log();
+
+console.log(a[0]);  // Hello
+console.log(a[a.length - 1]);   // 100
+
+console.log();
+
+for(const i of a)
+{
+    console.log(i);
+}
+
