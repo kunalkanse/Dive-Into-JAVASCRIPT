071469cb80605ffae1a719a5b322274a9e6b6aa2
kunal kanse <92772714+kunalkanse@users.noreply.github.com>
2024-01-10 17:46:15 +0530

    Update primitives_vs_Objects.js

--- 03_Datatypes/primitives_vs_Objects.js
+++ 03_Datatypes/primitives_vs_Objects.js
@@ -7,13 +7,18 @@ age = 31;
 console.log(age);  //31
 console.log(oldAge);  // 30
 
-////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////
 // primitive datatypes allocate memory into stack.
 // if one variable value passed to another variable
 // changes affect to only that particular variable.
-///////////////////////////////////////////////////
-// Objects
+//////////////////////////////////////////////////////////////////////////////////////////
+// non-primitives datatypes allocate memory in Heap.
+// gives reference of original value if passing to another variable
+// changes will affect to both variables, because both references points to same data.
+//////////////////////////////////////////////////////////////////////////////////////////
 
+
+// Objects
 const me = {
     name: 'Jonas',
     age: 30,
@@ -33,8 +38,30 @@ marvel.push("Kunal");
 
 console.log(DC);
 
-//////////////////////////////////////////////////////////////////////////////////////////
-// non-primitives datatypes allocate memory in Heap.
-// gives reference of original value if passing to another variable
-// changes will affect to both variables, because both references points to same data.
-//////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////
+// Exmple 2 ) 
+
+const flight = 'KSK56';
+
+const passenger = {
+    name: 'kunal kanse',
+    passport: 123645798
+}
+
+const checIn = function (flightNum, passenger) {
+    flightNum = 'LH989';
+    passenger.name = 'mr.' + passenger.name;
+
+    if (passenger.passport === 123645798) {
+        alert('Check in');
+    } else {
+        alert('Wrong Passport...!!!');
+    }
+}
+
+checIn(flight, passenger);
+
+console.log(flight);    // Original alue not changed
+console.log(passenger); // Original value changed
+
