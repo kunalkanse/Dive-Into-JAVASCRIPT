75c5776e896a8c774ec7562a214530aadae37ecd
kunal kanse <92772714+kunalkanse@users.noreply.github.com>
2024-01-27 23:47:59 +0530

    Create 02_basicArrayMethods.js

---
+++ 07_Arrays/01_Array_Detailed/02_Array_Methods/02_basicArrayMethods.js
@@ -0,0 +1,15 @@
+
+///////////////////////////////////////////////////////////////
+// ? 
+
+const arr = [23, 11, 64];
+
+console.log(arr[1]);    // 11
+console.log(arr.at(1)); // 11
+
+console.log(arr.at(arr.length - 1));    // 64
+console.log(arr.at(-1));    // 64
+console.log(arr.slice(-1)[0]); // 64
+
+console.log('kunal'.at(0)); // k
+console.log('kunal'.at(-2));    // a
