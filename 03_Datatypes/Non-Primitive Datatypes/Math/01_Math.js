ee153c94b629615d0037480c25f3d0dee287b96d
kunal kanse <92772714+kunalkanse@users.noreply.github.com>
2024-02-04 23:50:15 +0530

    Create 01_Math.js

---
+++ 03_Datatypes/Non-Primitive Datatypes/Math/01_Math.js
@@ -0,0 +1,24 @@
+
+let a = Math.random()
+console.log(a);
+
+let b = Math.round(5.4)
+console.log(b);
+
+let c = Math.floor(5.6)
+console.log(c);
+
+let d = Math.ceil(5.1)
+console.log(d);
+
+let e = Math.pow(4,2)
+console.log(e);
+
+let f = Math.sqrt(16)
+console.log(f);
+
+let g = Math.max(45,78,98,56,97)
+console.log(g);
+
+let h = Math.min(65,78,79,89,64)
+console.log(h);
