c50c3dd25f075e10a78561490db024684b3597bd
kunal kanse <92772714+kunalkanse@users.noreply.github.com>
2024-02-09 23:32:50 +0530

    Create index.js

---
+++ 08_Functions/01_Basic Syntax/01_Anonymous_Function/index.js
@@ -0,0 +1,14 @@
+
+// function(a,b)
+// {
+//     console.log(`${a+b}`);
+// }
+
+(10, 20)     // ERROR
+
+// To access anonymous function we need to assign it to variable i.e in function with expression
+// and call using variable name  => add(10,20)
+
+// or
+
+// we can access using it as an argument to another function i.e as callback function
