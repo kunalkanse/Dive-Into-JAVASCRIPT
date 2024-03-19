f778c3545c948fd56f11a4801fe997707ec702a3
kunal kanse <92772714+kunalkanse@users.noreply.github.com>
2024-03-19 23:36:29 +0530

    Create 01_Object.js

---
+++ 09_Objects/03_Object/01_Object.js
@@ -0,0 +1,16 @@
+
+// Array
+const kunalInfoArr = ['Kunal', 'Kanse', 22, 'Java Full Stack developer', ['Aditya', 'Omkar', 'Kunal']];
+
+// Object
+const kunalInfoObj = {
+    firstName: 'Kunal',
+    lastName: 'Kanse',
+    age: 22,
+    Job: 'Java Full Stack developer',
+    friends: ['Aditya', 'Omkar', 'Kunal']
+}
+
+console.log(kunalInfoArr);
+console.log();
+console.log(kunalInfoObj);
