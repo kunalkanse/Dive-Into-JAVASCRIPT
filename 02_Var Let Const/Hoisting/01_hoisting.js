e254db7292163d4f2d6ab9d0f8d23be476b4df1e
kunal kanse <92772714+kunalkanse@users.noreply.github.com>
2024-03-20 23:45:48 +0530

    Create 01_hoisting.js

---
+++ 02_Var Let Const/Hoisting/01_hoisting.js
@@ -0,0 +1,42 @@
+
+console.log(firstName); //undefined
+// console.log(job);
+// console.log(age);
+
+var firstName = 'Kunal';
+
+let job = 'developer';
+const age = 23;
+
+//ERR : Cannot access before initialization
+
+////////////////////////////////////////
+
+console.log(add1(2, 3));    // 5
+
+console.log(add2);  // undefined
+// console.log(add2(2,3)); // undefined(2,3), 
+
+console.log(add3);  // undefined// 
+// console.log(add3(2,3)); // undefined(2,3),
+
+function add1(a, b) {
+    return a + b;
+}
+
+var add2 = function (a, b) {
+    return a + b;
+}
+
+var add3 = (a, b) => a + b;
+
+//////////////////////////////////
+// Cannot access 'add2' before initialization
+
+// const add2 = function (a, b) {
+//     return a + b;
+// }
+
+// let add3 = (a, b) => a + b;
+
+console.log(!undefined);    // true
